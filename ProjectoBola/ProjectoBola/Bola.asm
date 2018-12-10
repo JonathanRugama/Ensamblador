dba653818cdf4af238366923ec45cbc89a53fe61
Jonathan Rugama <39573472+JonathanRugama@users.noreply.github.com>
2018-12-10 00:40:57 -0600

    Cambios de Andres
    - Contadores

--- ProjectoBola/ProjectoBola/Bola.asm
+++ ProjectoBola/ProjectoBola/Bola.asm
@@ -10,17 +10,12 @@ INCLUDELIB c:\Irvine\Irvine32.lib
 		.data		;Directiva de Inicio de Variables.
 
 TBox	db			"Ball Game",0	;Título de Ventana.
-TgoJA	byte		"Game Over - Gana Jugador A",0
-TgoJB	byte		"Game Over - Gana Jugador B",0
+TgoJA	byte		"Game Over - Gana Jugador Andres",0
+TgoJB	byte		"Game Over - Gana Jugador Juan",0
 TgoVJ	byte		"¿Desea volver a jugar?",0
 Ball	byte		'0'
-
-
-Col		byte		117		;Columna de Bola.
-Fil		byte		0		;Fila de Bola.
-
-
-
+Col		byte		1		;Columna de Bola.
+Fil		byte		10		;Fila de Bola.
 Maxc	byte		0		;Columna Máxima.
 Maxf	byte		0		;Fila Máxima.
 Dcol	byte		0		;Dirección Columna. (0 = Derecha, 1 = Izquierda).
@@ -29,28 +24,8 @@ Raf		byte		10		;Fila de Raqueta A.
 Rac		byte		0		;Columna Raqueta A.
 Rbf		byte		10		;Fila de Raqueta B.
 Rbc		byte		0		;Columna Raqueta B.
-choca	byte		0
-hayDosBolas	byte	0		;Pregunta si hay una bola
-TgoE	byte		"Entra"
-
-DcolB	byte		0		;Dirección Columna. (0 = Derecha, 1 = Izquierda).
-DfilB	byte		0		;Dirección Fila. (0 = Abajo, 1 = Arriba).
-
-
-;PILA DE VARIABLES para dibujar la pelota
-pelota_parte1	byte	" .. "           ,0		
-pelota_parte2	byte    "::::"          ,0
-pelota_parte3	byte	" '' "    ,0	;FORMA DE LA BOLA
-
-;PILA DE VARIABLES para borrar la pelota
-Bpelota_parte1	byte	"    "           ,0		
-Bpelota_parte2	byte    "    "          ,0
-Bpelota_parte3	byte	"    "    ,0	;FORMA DE LA BOLA
-
-pelota_fila      byte    10	
-pelota_columna   byte    1 
-Fila_maxima byte 0					;Fila maxima para la creacion de la pelota
-Columna_max byte 0					;columna maxima para la creacion de la pelota
+suma	byte		'9'
+suma1	byte		'9'
 
 		.code
 
@@ -63,7 +38,7 @@ main	PROC
 Inicio: call		GetMaxXY	;Toma el Tamaño Actual de la Ventana de Consola.
 		mov			Maxc,dl
 		mov			Rbc,dl
-		mov			Maxf,21
+		mov			Maxf,23
 		sub			Maxc,2
 		sub			Rbc,1
 		mov			dh,0		;Escribe Línea Superior.
@@ -72,45 +47,26 @@ Inicio: call		GetMaxXY	;Toma el Tamaño Actual de la Ventana de Consola.
 		mov			dh,24		;Escribe Línea Inferior.
 		mov			dl,0
 		call		WLin
+		call		WRaya
 		call		Wjuga		;Escribe Identificadores para Jugadores.
+		call        Wconta      ;Escribe contadores para jugadores
 		mov			edx,0		;Vuelve Cursos al Inicio.
 		call		Gotoxy
-	
 
-Ciclo:		
-		call		DibujarPelota
+Ciclo:	call		Wball		;Escribe la Bola.
 		mov			dh,Raf		;Escribe Raqueta A.
 		mov			dl,Rac
 		call		WRaq
 		mov			dh,Rbf		;Escribe Raqueta B.
 		mov			dl,Rbc
 		call		WRaq
-		mov			eax,30	;Espera de 200 ms (0.2 s).
+		mov			eax,50		;Espera de 200 ms (0.2 s).
 		call		Delay
-		call		Borra_pelota
-		;call		CicloB
-		call		ReadKey		;Revisa el Teclado.
-		jz			Ccol		;Si no hay tecla, salto a Control Columna.
-		jz			CcolB		;Si no hay tecla, salto a Control Columna................................................
-		jnz			Tecla		;Si hay tecla.
-
-CicloB:
-		call		Wball		;Crea una nueva bola
-		mov			dh,Raf		;Escribe Raqueta A.
-		mov			dl,Rac
-		call		WRaq
-		mov			dh,Rbf		;Escribe Raqueta B.
-		mov			dl,Rbc
-		call		WRaq
-		mov			eax,50    ;Espera de 200 ms (0.2 s).
-		call		Delay		;Detiene la ejecución del programa durante un intervalo especifi cado de n milisegundos
 		call		CBall		;Borra la Bola.
 
 		call		ReadKey		;Revisa el Teclado.
 		jz			Ccol		;Si no hay tecla, salto a Control Columna.
-		jz			CcolB		;Si no hay tecla, salto a Control Columna..........................................................
 		jnz			Tecla		;Si hay tecla.
-
 
 Tecla:	cmp			al,'a'		;Si Tecla es 'a' --> Raqueta 1 Arriba.
 		je			RaUp
@@ -157,135 +113,97 @@ RbDw:	mov			dh,Rbf		;Borro Raqueta B posición Actual.
 Ccol:	cmp			Dcol,0		;Reviso Dirección Movimiento de Columna.
 		jne			Cup
 
-CcolB:	cmp			Dcol,0		;Reviso Dirección Movimiento de Columna.
-		jne			Cup
-
-
-Cdown:	
-		mov			al,pelota_columna		;Columna se Incrementa
-		add			al,3
+Cdown:	mov			al,Col		;Columna se Incrementa
 		cmp			al,Maxc
 		jnl			CDcol1
-		inc			pelota_columna
+		inc			Col
 		jmp			Cfil
 
-
-
-Cup:	cmp			pelota_columna,1
+Cup:	cmp			Col,1
 		jng			CDcol2
-		dec			pelota_columna
+		dec			Col
 		jmp			Cfil
-
-
 
 CDcol1:	mov			al,Rbf		;Reviso si hay choque con Raqueta B.
-		cmp			al,pelota_fila
-	
+		cmp			al,Fil
+		jg			GameO1		;Salto a Game Over.
 		add			al,3
-		cmp			al,pelota_fila
-	    ;sub			al,2  ;Le resto dos a al
-		mov			Fil,al ;Sirve para hacer que la bola aparezca en el centro de la raqueta
-		
+		cmp			al,Fil
+		jl			GameO1		;Salto a Game Over.
 		mov			Dcol,1		;Cambia Dirección de Movimiento en Columna.
-		
-		
-		mov			al,hayDosBolas
-		.IF hayDosBolas == 0 ;Si es igual a 0 se crea una nueva bola
-			
-		cmp		al,hayDosBolas  ;Compara si es igual a 0
-		
-		jmp			CicloB		
 		jmp			Cfil
-		
-		.ELSE			;En caso contrario no se crea
-		jmp			Cfil
-		.ENDIF
 
 CDcol2:	mov			al,Raf		;Reviso si hay choque con Raqueta A.
-		cmp			al,pelota_fila
-	
+		cmp			al,Fil
+		jg			GameO2		;Salto a Game Over.
 		add			al,3
-		cmp			al,pelota_fila
-	
+		cmp			al,Fil
+		jl			GameO2		;Salto a Game Over.
 		mov			Dcol,0		;Vuelve cursor a inicio de fila.
 		jmp			Cfil
 
 Cfil:	cmp			Dfil,0		;Reviso Dirección de Movimiento en Fila.
-		;cmp			DfilB,1
 		jne			Fup
-		
 
-Fdown:	mov			al,pelota_fila		;Columna se Incrementa
+Fdown:	mov			al,Fil		;Columna se Incrementa
 		cmp			al,Maxf
 		jnl			CDfil1
-		inc			pelota_fila
-		call		Ciclo
-		call		CicloB
+		inc			Fil
+		jmp			Ciclo
 
-Fup:	cmp			pelota_fila,1
-		;cmp		Fil,117
+Fup:	cmp			Fil,1
 		jng			CDfil2
-		dec			pelota_fila
-		;dec		Fil
-		call		Ciclo
-		call		CicloB
-
+		dec			Fil
+		jmp			Ciclo
 
 CDfil1:	mov			Dfil,1		;Cambia Dirección de Movimiento en Fila.
-		mov		DfilB,0
-		call		Ciclo
-		call		CicloB
+		jmp			Ciclo
 
 CDfil2:	mov			Dfil,0		;Vuelve cursor a inicio de fila.
-	    mov			DfilB,1
-		call		Ciclo
-		call		CicloB
+		jmp			Ciclo
 
-
-
-GameO1: call		Borra_pelota		;Borro la Bola. (Pierde Raqueta B).
-		mov			dh,Rbf		;Escribe Raqueta B.
-		mov			dl,Rbc
-		call		WRaq
-		inc			Col
-		inc			pelota_columna			;Incrementando Columna.
-		mov			al,Dfil		;Reviso Dirección de Movimiento de la Bola.
-		cmp			al,0
-		jne			Posf1
-		add			Fil,2
-		add			pelota_fila,2		;Ajuste de Fila para Mostrar Final de Bola.
-Posf1:	sub			pelota_fila,1
-		sub			Fil,1
-		call		Wball
-		call		DibujarPelota
-		mov			edx,OFFSET TgoJA
-		jmp			Final
-		
-GameO2: call		Borra_pelota		;Borro la Bola. (Pierde Raqueta A).
+GameO1: call		CBall		;Borro la Bola. (Pierde Raqueta A).
 		mov			dh,Raf		;Escribe Raqueta A.
 		mov			dl,Rac
 		call		WRaq
-		dec			pelota_columna			;Decrementando Columna.
+		dec			Col			;Decrementando Columna.
 		mov			al,Dfil		;Reviso Dirección de Movimiento de la Bola.
 		cmp			al,0
 		jne			Posf2
-		add			Fil,2
-		add			pelota_fila,2		;Ajuste de Fila para Mostrar Final de Bola.
-Posf2:	sub			pelota_fila,1
-		sub			Fil,1
-		call		Wball
-		call		DibujarPelota
+		add			Fil,2		;Ajuste de Fila para Mostrar Final de Bola.
+Posf1:	sub			Fil,1
+		call		WBall
 		mov			edx,OFFSET TgoJB
-		jmp			Final
+		dec			suma1
+		call		iniciar
+		jmp			inicio
 		
+GameO2: call		CBall		;Borro la Bola. (Pierde Raqueta A).
+		mov			dh,Raf		;Escribe Raqueta A.
+		mov			dl,Rac
+		call		WRaq
+		dec			Col			;Decrementando Columna.
+		mov			al,Dfil		;Reviso Dirección de Movimiento de la Bola.
+		cmp			al,0
+		jne			Posf2
+		add			Fil,2		;Ajuste de Fila para Mostrar Final de Bola.
+Posf2:	sub			Fil,1
+		call		WBall
+		mov			edx,OFFSET TgoJB
+		dec			suma1
+		call		iniciar
+		jmp			inicio
+		
+		
+		
+
 Final:	mov			ebx,OFFSET TBox	 ;Muestra Ventaja de Game Over.
 		call		MsgBox
 		mov			edx,OFFSET TgoVJ ;Muestra Ventana de Volver a Jugar.
 		call		MsgBoxAsk
 		cmp			eax,6			 ;Si valor retornado en EAX no es 6,
 		jne			Fout			 ;se sale del juego.
-		call		CRaq
-		call		Borra_pelota			 ;Borra Bola de Posición Actual.
+		call		CBall			 ;Borra Bola de Posición Actual.
 		mov			dh,Raf			 ;Borro Raqueta A posición Actual.
 		mov			dl,Rac
 		call		CRaq
@@ -296,29 +214,38 @@ Final:	mov			ebx,OFFSET TBox	 ;Muestra Ventaja de Game Over.
 		jmp			Inicio			 ;Salta a juego nuevo.
 
 Fout:	exit
-
-
 		
 main	ENDP
 
+;*************************************************************************************************
+; PROCEDIMIENTO INICIAR - Inicializa Variables.
+;*************************************************************************************************
+
+
+
+Iniciar	PROC
+		call		CBall
+		mov			Col,60		;Inicializa Columna de Bola.
+		mov			Fil,10		;Inicializa Fila de Bola.
+	
+		RET						;Retorno de Procedimiento.
+
+Iniciar	ENDP
 ;*************************************************************************************************
 ; PROCEDIMIENTO INIVAR - Inicializa Variables.
 ;*************************************************************************************************
 
 Inivar	PROC
 
-		mov			pelota_columna,1		;Inicializa Columna de Bola.
-		mov			pelota_fila,10		;Inicializa Fila de Bola.
+		mov			Col,1		;Inicializa Columna de Bola.
+		mov			Fil,10		;Inicializa Fila de Bola.
 		mov			Dcol,0		;Inicializa Dirección Avance Columna de Bola.
 		mov			Dfil,0		;Inicializa Dirección Avance Fila de Bola.
-		mov			DcolB,1	;Dirección Columna. (0 = Derecha, 1 = Izquierda).
-		mov      	DfilB,1	;Dirección Fsila. (0 = Abajo, 1 = Arriba).
-	
 		mov			Raf,10		;Inicializa Fila de Raqueta A.
 		mov			Rac,0		;Inicializa Columna de Raqueta A.
 		mov			Rbf,10		;Inicializa Fila de Raqueta B.
 		mov			Rbc,0		;Inicializa Columna de Raqueta B.
-		mov			hayDosBolas,0 ;Inicializa la variable	
+	
 		RET						;Retorno de Procedimiento.
 
 Inivar	ENDP
@@ -336,27 +263,102 @@ Wjuga	PROC
 		call		Gotoxy
 		mov			al,'A'
 		call		WriteChar
-		mov			dl,Maxc
-		sub			dl,3
-		call		Gotoxy
-		mov			al,'B'
+		mov			al,'n'
 		call		WriteChar
-		mov			eax,white + (black *16)
-		call		SetTextColor
-	
+		mov			al,'d'	
+		call		WriteChar
+		mov			al,'r'
+		call		WriteChar
+		mov			al,'e'
+		call		WriteChar
+		mov			al,'s'
+		call		WriteChar
+		mov			dl,Maxc
+		sub			dl,7
+		call		Gotoxy
+		mov			al,'J'
+		call		WriteChar
+		mov			al,'u'
+		call		WriteChar
+		mov			al,'a'
+		call		WriteChar
+		mov			al,'n'
+		call		WriteChar
+
+
+
 		RET						;Retorno de Procedimiento.
 
 Wjuga	ENDP
 
+;*************************************************************************************************
+; PROCEDIMIENTO WJUGA - Escribe Contadores para Jugadores A y B
+;*************************************************************************************************
+
+Wconta	PROC
+
+		mov			eax,red + (green )
+		call		SetTextColor
+		mov			dh,1
+		mov			dl,6
+		call		Gotoxy
+		mov			al,suma1
+		call		WriteChar
+		mov			dl,Maxc
+		sub			dl,5
+		call		Gotoxy
+		mov			al,suma
+		call		WriteChar
+		call		SetTextColor
+	
+		RET						;Retorno de Procedimiento.
+
+Wconta	ENDP
+
+;*************************************************************************************************
+; PROCEDIMIENTO WBALL - Escribe la Bola
+;*************************************************************************************************
+
+Wball	PROC
+
+		mov			eax,yellow + (black * 16)
+		call		SetTextColor
+		mov			dh,Fil		;Se pasa la Fila.
+		mov			dl,Col		;Se pasa la Columna.
+		call		Gotoxy		;Posiciona Cursor en (Fil,Col).
+		mov			al,Ball		;Escribe un '0'.
+		call		WriteChar
+		mov			eax,white + (black * 16)
+		call		SetTextColor
+		
+		RET						;Retorno de Procedimiento.
+
+Wball	ENDP
+
+;*************************************************************************************************
+; PROCEDIMIENTO CBALL - Borra la Bola
+;*************************************************************************************************
+
+Cball	PROC
+
+		mov			dh,Fil		;Se pasa la Fila.
+		mov			dl,Col		;Se pasa la Columna.
+		call		Gotoxy		;Posiciona Cursor en (Fil,Col).
+		mov			al,' '		;Escribe un ' '.
+		call		WriteChar
+		
+		RET						;Retorno de Procedimiento.
+
+Cball	ENDP
 
 ;*************************************************************************************************
 ; PROCEDIMIENTO WRAQ - Escribe Raqueta (Parámetros: Dh --> Fila y Dl --> Columna)
 ;*************************************************************************************************
 
 WRaq	PROC
-		
+
 		mov			ebx,4		;Contador.
-		mov			al,'X'		;Caracter.
+		mov			al,'|'		;Caracter.
 WRaqc:	call		Gotoxy		;Posicionar Cursor.
 		call		WriteChar	;Escribir Caracter.
 		inc			dh			;Incrementar Columna.
@@ -367,10 +369,22 @@ WRaqc:	call		Gotoxy		;Posicionar Cursor.
 
 Wraq	ENDP
 
+WRaya	PROC
+		mov			eax,yellow + (black *16)
+		call		SetTextColor
+		mov			dh,1
+		mov			dl,60
+		mov			ebx,23		;Contador.
+		mov			al,'|'		;Caracter.
+WRaqc:	call		Gotoxy		;Posicionar Cursor.
+		call		WriteChar	;Escribir Caracter.
+		inc			dh			;Incrementar Columna.
+		dec			ebx			;Decrementar Contador.
+		jnz			Wraqc		;Si no es cero, salto a ciclo de raqueta.
+		
+		RET						;Retorno de Procedimiento.
 
-
-
-
+Wraya	ENDP
 
 ;*************************************************************************************************
 ; PROCEDIMIENTO CRAQ - Borra Raqueta (Parámetros: Dh --> Fila y Dl --> Columna)
@@ -401,7 +415,7 @@ WLin	PROC
 		mov			ebx,0		;Contador en 32 bis.
 		mov			bl,Maxc
 		add			bl,2
-		mov			al,'*'		;Caracter.
+		mov			al,'-'		;Caracter.
 WLinc:	call		Gotoxy		;Posicionar Cursor.
 		call		WriteChar	;Escribir Caracter.
 		inc			dl			;Incrementar Columna.
@@ -413,129 +427,6 @@ WLinc:	call		Gotoxy		;Posicionar Cursor.
 		RET						;Retorno de Procedimiento.
 
 WLin	ENDP
-
-
-
-;*************************************************************************************************
-; PROCEDIMIENTO Borrar_pelota - Escribe Línea de Caracteres (Parámetros: Dh --> Fila y Dl --> Columna)
-;*************************************************************************************************
-   
-Borra_pelota PROC
-        mov     eax,white + (black * 16)
-        call    SetTextColor
-        mov     dh,pelota_fila                       ;Se pasa la Fila
-        mov     dl,pelota_columna                    ;Se pasa la Columna
-        call    GotoXY                              ;Posiciona cursor en (Auto1_fila,Auto1_columna)
-        mov     ecx,OFFSET Bpelota_parte1             ;Dibujar Auto1 (por segmentos)
-        call    Dibujar_pelotaXpartes
-        RET
-Borra_pelota ENDP
-
-
-
-;*************************************************************************************************
-; PROCEDIMIENTO Dibujar_pelota - Escribe Línea de Caracteres (Parámetros: Dh --> Fila y Dl --> Columna)
-;*************************************************************************************************
-   
-   
-DibujarPelota PROC
-        mov     eax,yellow + (black * 16)
-        call    SetTextColor
-        mov     dh,pelota_fila                       ;Se pasa la Fila
-        mov     dl,pelota_columna                    ;Se pasa la Columna
-        call    GotoXY                              ;Posiciona cursor en (Auto1_fila,Auto1_columna)
-        mov     ecx,OFFSET pelota_parte1             ;Dibujar Auto1 (por segmentos)
-        call    Dibujar_PelotaXpartes
-        RET
-DibujarPelota ENDP
-
-;*************************************************************************************************
-; PROCEDIMIENTO Dibujar_pelotaXpartes - Escribe Línea de Caracteres (Parámetros: Dh --> Fila y Dl --> Columna)
-;*************************************************************************************************
- 
-
-Dibujar_PelotaXpartes PROC
-        mov     eax,white + (black * 16)
-        call    SetTextColor
-        mov     pelota_fila,dh                           ;Guarda cordenadas seleccionadas
-        mov     pelota_columna,dl
-        call    GetMaxXY                                ;Toma tamaño de la consola
-        mov     Fila_maxima,al
-        mov     Columna_max,dl
-        cmp     al,pelota_fila                           ;se compara si coordenadas son calidas
-        jl			GoOut
-        cmp     dl,pelota_columna
-        jl      GoOut
-        mov     dh,pelota_fila                           ;Posicion inicial del cursor
-        mov     dl,pelota_columna
-
-        call    GotoXY                                  ;Posicionando el cursor
-        push    edx                                     ;guarda edxen la pila
-        mov     edx,ecx                                 ;Escribiendo linea 1 del auto1
-        call    writeString
-        pop     edx                                     ;Sacando edx de la pila
-
-        inc			dh									; Ajustando Fila y Reposicionando.    ;esto mismo en los siguientes
-		add			ecx,5								; Paso a Siguiente Fila.
-		call		GotoXY
-		push		edx									; Guardando EDX en la Pila.
-		mov			edx, ecx							; Escribiendo Línea.
-		call		WriteString
-		pop			edx									; Sacando EDX de la Pila.
-
-
-        inc			dh									
-		add			ecx,5								
-		call		GotoXY
-		push		edx									
-		mov			edx, ecx							
-		call		WriteString
-		pop			edx									
-
-
-GoOut:	nop							; No operación.
-
-    RET
-Dibujar_PelotaXpartes ENDP
-
-
-
-;*************************************************************************************************
-; PROCEDIMIENTO WBALL - Escribe la Bola
-;*************************************************************************************************
-
-Wball	PROC
-
-		mov			hayDosBolas,1 ;Asigna uno para que solo se cree una bola
-		mov			eax,red + (black * 16)
-		call		SetTextColor
-		mov			dh,Fil		;Se pasa la Fila.
-		mov			dl,Col		;Se pasa la Columna.
-		call		Gotoxy		;Posiciona Cursor en (FilB,ColB).
-		mov			al,Ball		;Escribe un '0'.
-		call		WriteChar
-	
-	
-		
-		RET						;Retorno de Procedimiento.
-
-Wball	ENDP
-
-;*************************************************************************************************
-; PROCEDIMIENTO CBALL - Borra la Bola
-;*************************************************************************************************
-
-Cball	PROC
-
-		mov			dh,Fil		;Se pasa la Fila.
-		mov			dl,Col		;Se pasa la Columna.
-		call		Gotoxy		;Posiciona Cursor en (FilB,ColB).
-		mov			al,' '		;Escribe un ' '.
-		call		WriteChar
-		
-		RET						;Retorno de Procedimiento.
-
-Cball	ENDP
 
 
 		END
